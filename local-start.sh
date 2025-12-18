15a38747fee6fbf700ad4b4b137bb4d8fa3077d1
Yufei Wang <yff.wang@outlook.com>
2025-12-18 13:17:17 +0800

    add sleep time for node deployment for CI

--- local-start.sh
+++ local-start.sh
@@ -3,4 +3,4 @@ chmod +x ./node-bin/eth-rpc-linux-x64
 ./node-bin/revive-dev-node-linux-x64 --dev --tmp --unsafe-rpc-external &>node-logs/node.log &
 sleep 1
 ./node-bin/eth-rpc-linux-x64 &>node-logs/rpc.log &
-sleep 2
+sleep 20
